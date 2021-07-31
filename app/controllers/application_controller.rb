f787f15057b8aff4bd33f25081a68fda490b9abf
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2021-07-31 08:49:31 +0000

    FINISH

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,19 +9,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number])
   end
 
-  def after_inactive_sign_up_path_for(resource_or_scope)
-    if resource_or_scope.is_a?(Admin)
-        admin_path(resource_or_scope)
-    else
-        customer_path(resource_or_scope)
-    end
-  end
-
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
         admin_path
     else
+      if params[:commit] == "新規登録"
+        customer_path(resource_or_scope)
+      else
         root_path
+      end
     end
   end
 
